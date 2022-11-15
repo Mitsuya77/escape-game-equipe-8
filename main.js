db7198194acfca5dfd98edfc2ec60ad248a2476c
Mouchon <william.mouchon@edu.devinci.fr>
2022-11-15 11:46:34 +0100

    js chrono

---
+++ main.js
@@ -0,0 +1,17 @@
+// Compte a reboure 
+const departMinutes = 10
+let temps = departMinutes * 60
+//  récuperation de l'id
+const timerElement = document.getElementById("timer")
+
+//  appelle de manière répétée la fonction
+setInterval(() => {
+  let minutes = parseInt(temps / 60, 10)
+  let secondes = parseInt(temps % 60, 10)
+
+  minutes = minutes < 10 ? "0" + minutes : minutes
+  secondes = secondes < 10 ? "0" + secondes : secondes
+
+  timerElement.innerText = `${minutes}:${secondes}`
+  temps = temps <= 0 ? 0 : temps - 1
+}, 1000)
